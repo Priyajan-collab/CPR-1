5fc32f87f246fa8b9454ba9940faf13be8dd93d8
Priyajan-collab <68772479+Priyajan-collab@users.noreply.github.com>
2020-07-25 20:01:48 +0545

    game patrol
    left to right

---
+++ Our game/Assets/patrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+    public float speed;
+    public bool movementright = true;
+    public Transform grounddetection;
+    public float length; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(grounddetection.position, Vector2.down, length);
+        if (groundCheck.collider == false)
+        {
+            if (movementright == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movementright = false;
+
+
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movementright = true;
+            }
+
+        }
+
+
+
+    }
+}
